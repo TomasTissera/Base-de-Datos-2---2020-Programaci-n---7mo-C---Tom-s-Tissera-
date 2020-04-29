0a7f335aa1a10014228d005c8892d9b2c472fec9
Tomás Tissera <tomastissefa@gmail.com>
2020-04-29 11:26:33 -0300

    Update class_06.sql
    correccion de ejercicios

--- class_06.sql
+++ class_06.sql
@@ -3,20 +3,28 @@ select *from actor a , actor a2
     where a.last_name <> a2.last_name;
 
 --2-Encuentra actores que no funcionan en ninguna película.
-
+SELECT * FROM actor a 
+    WHERE NOT EXISTS (
+        SELECT actor_id 
+        FROM film_actor 
+        where a.actor_id = fa.actor_id 
+        AND film_id = film_id);
 
 --3-Encuentra clientes que alquilaron solo una película
-select * from  customer c2 
-    where active = 0;
+SELECT * FROM customer c 
+    WHERE(
+        SELECT COUNT(*) 
+        FROM rental r2 
+        WHERE r2.customer_id = c.customer_id) = 1;
 
 --4-Encuentra clientes que alquilaron más de una película
 SELECT * FROM customer c 
     WHERE (SELECT COUNT(*) 
-    FROM rental r2 
-    WHERE r2.customer_id = c.customer_id) = 1;
+        FROM rental r2 
+        WHERE r2.customer_id = c.customer_id) > 1;
 
 --5-Enumere los actores que actuaron en 'BETRAYED REAR' o en 'CATCH AMISTAD'
-SELECT * FROM actor a, film_actor fa , film f 
+SELECT fitst_name , last_name FROM actor a, film_actor fa , film f 
     WHERE a.actor_id=fa.actor_id
 	AND f.film_id = fa.film_id 
     AND (f.title = "BETRAYED REAR" OR f.title = "CATCH AMISTAD");
@@ -27,7 +35,8 @@ SELECT * FROM actor a, film_actor fa , film f
 	AND f.film_id=fa.film_id 
     AND f.title="BETRAYED REAR" 
     AND a.actor_id NOT IN (
-		SELECT a2.actor_id from actor a2, film_actor fa2, film f2 
+		SELECT a2.actor_id 
+        from actor a2, film_actor fa2, film f2 
         WHERE a2.actor_id = fa2.actor_id 
 		AND f2.film_id=fa2.film_id 
         AND f2.title="CATCH AMISTAD"
