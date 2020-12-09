3101682d94d6a86e7a529ae9498f305daaf51047
Tomás Tissera <tomastissefa@gmail.com>
2020-12-08 23:23:36 -0300

    Update class_21.py

--- class_21.py
+++ class_21.py
@@ -1,24 +1,26 @@
 import redis
-from datetime import date
-r = redis.Redis(host='localhost', port=6000)
-print("Bienvenido a tu sistema de guardado de Gastos")
-gasto = 'a'
-today = str(date.today())
-fecha = 'a'
-while gasto != "":
-    gasto = input("Inserte su Gasto mas reciente o presione enter para salir ")
-    if gasto == "":
-        pass
+
+REDIS_PORT = 7000
+REDIS_HOST = 'localhost'
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+a = True
+
+while a:
+
+    print('que quiere hacer? add/show/exit')
+    res1 = input()
+
+    if res1 == 'add':
+        print("Ingrese nombre de la comida:")
+        nombre = input()
+        print("Ingrese el tipo de comida que es:")
+        tipo = input()
+        r.set(nombre, tipo)
+    elif res1 == 'show':
+        print("Todas las comidas:")
+        print(r.keys())
+    elif res1 == 'exit':
+        a = False
     else:
-        r.lpush(today, gasto)
-print('Los Gastos del dia ' + today+' fueron:')
-for a in r.lrange(today, 0, -1):
-    print(str(a.decode('utf-8')))
-print('Estas son todas las fechas que en las que se guardaron Gastos: ')
-for key in r.scan_iter():
-    print(str(key.decode('utf-8')))
-while fecha != '':
-    fecha = input(
-        'Inserte la fecha de la que desea saber que Gasto o presione enter para salir ')
-    for x in r.lrange(fecha, 0, -1):
-        print(str(x.decode('utf-8')))
+        print('Ingrese opcion valida')
